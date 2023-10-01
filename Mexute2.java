7df51893a11522ca9db4d99e4210f5d27bd4f96e
S <s>
2023-10-01 18:00:46 +0400

    {"types":"A","runs":12}

---
+++ Mexute2.java
@@ -0,0 +1,16 @@
+import stanford.karel.*;
+public class Mexute2 extends SuperKarel{
+	public void run(){
+		move();
+	
+		
+	}
+	private void Davigale(){
+		for (int i=0; i<n; i++){
+			
+		}
+			
+	}
+}
+
+
