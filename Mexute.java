64c860489c4e09347fde986c6ef07f9d95145a33
S <s>
2023-10-01 19:04:54 +0400

    {"types":"C","runs":13}

--- Mexute.java
+++ Mexute.java
@@ -2,15 +2,28 @@ import stanford.karel.*;
 public class Mexute extends SuperKarel{
 	public void run(){
 		move();
-	
 		
-	}
-	private void Davigale(){
-		for (int i=0; i<n; i++){
-			
+	while(noBeeperPresent())
+	       {pickBeeper();
 		}
+	
+	for (int i=0; i<n; i++){
+			pickBeeper();
+			}
+		
+		move();
+		int m=beepersPresent();
+		for (int i=0; i<m; i++){
+			pickBeeper();
+			}
+		move();
+		int x=m+n
+		for (int i=0;i<x; i++){
+			putBeeper();
+		}	
 			
 	}
+		
 }
 
 
