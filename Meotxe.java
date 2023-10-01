db7d7670ee8102cf69e929162f03c65086e6a60d
S <s>
2023-10-01 17:29:54 +0400

    {"types":"AR","runs":2}

---
+++ Meotxe.java
@@ -0,0 +1,16 @@
+import stanford.karel.*;
+
+public class Meotxe extends SuperKarel{
+	public void run(){
+		daarige();
+		
+		
+	}
+	private void daarige(){
+		while(frontIsClear()){
+			putBeeper();
+			}
+	}
+}
+
+
