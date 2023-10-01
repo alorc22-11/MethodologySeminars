8cbac279d38548be2bb012b23c024d51b4389f69
S <s>
2023-10-01 16:56:18 +0400

    {"types":"C","runs":5}

--- Secondkarelhw.java
+++ Secondkarelhw.java
@@ -1,6 +1,22 @@
 import stanford.karel.*;
 
 public class Secondkarelhw extends SuperKarel{
-
+ public void run(){
+	 move();
+	 move();
+	 move();
+	 move();
+	  for(int i=0; i<10; i++){
+		  pickBeepeer();
+	  }
+	 move();
+	 for(int i=0; i<10; i++){
+		 putBeeper();
+	 }
+	 move();
+	 
+	  
+ }
+ 
 	
 }
