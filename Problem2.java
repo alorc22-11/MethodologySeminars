30aa2338b321e13615048b43752f9f77ad6d434e
S <s>
2023-10-03 18:27:41 +0400

    {"types":"AR","runs":0}

--- Problem2.java
+++ Problem2.java
@@ -1,11 +1,13 @@
 import stanford.karel.*;
 
-public class Secondkarelhw extends SuperKarel{
+public class Problem2 extends SuperKarel{
  public void run(){
 	 move();
 	 move();
 	 move();
 	 move();
+	 //shegvidzlia esec for it vqnat,anu for(int i=0; i<4;i++)move();
+	 
 	  for(int i=0; i<10; i++){
 		  pickBeeper();
 	  }
