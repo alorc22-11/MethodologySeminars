0e7b6cbecbb756070fc13836fe1833b481ae13b7
S <s>
2023-10-03 16:39:34 +0400

    {"types":"C","runs":5}

--- Meshvide.java
+++ Meshvide.java
@@ -1,4 +1,27 @@
-
-public class Meshvide {
+import stanford.karel.*;
+public class Meshvide extends SuperKarel{
+	public void run(){
+		putBeeper();
+		daarige();
+		mouxvie();
+		
+		
+	}
+	private void daarige(){
+		while(frontIsClear()){
+			move();
+			if(noBeepersPresent()){
+				putBeeper();
+			}
+			
+		}
+	}
+	private void mouxvie(){
+		if(frontIsClear()){
+			daarige();
+		}else{ turnLeft();
+		
+		}
+	}
 
 }
