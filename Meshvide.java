e446ee7a4430020f813f1069601de2d17f3537e1
S <s>
2023-10-03 16:30:45 +0400

    {"types":"C","runs":2}

--- Meshvide.java
+++ Meshvide.java
@@ -2,6 +2,21 @@ import stanford.karel.*;
 
 
 public class Meshvide extends SuperKarel{
+	public void run(){
+		
+	}
 	
+	private void Midi(){
+		while(frontIsClear()){
+			putBeeper();}
+		turnLeft();
+		
+		}
+	
+	}
 
+	
+		
+	}
+	
 }
