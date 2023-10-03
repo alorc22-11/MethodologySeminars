e61079c59dda6f55be057c83ec480164ebf4bc0f
S <s>
2023-10-03 17:02:19 +0400

    {"types":"C","runs":15}

--- Meotxe.java
+++ Meotxe.java
@@ -2,18 +2,20 @@ import stanford.karel.*;
 
 public class Meotxe extends SuperKarel{
 	public void run(){
+		putBeeper();
 		daarige();
 		
 		
 	}
 	private void daarige(){
-		if(noBeepersPresent()){
-			putBeeper();
-			
 		while(frontIsClear()){
 			move();/* eseigi 5zec mushaobs*/
 			
 			}
+		if(noBeepersPresent()){
+			putBeeper();
+			
+		
 		
 			}
 	}
