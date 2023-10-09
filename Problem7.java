439174adea6fd6b40a7cfbace9b4ac8f8aef3c22
S <s>
2023-10-09 23:09:21 +0400

    {"types":"C","runs":34}

--- Problem7.java
+++ Problem7.java
@@ -2,7 +2,7 @@ import stanford.karel.*;
 public class Problem7 extends SuperKarel{
 	public void run(){
 		 putBeeper();
-		 for(int s=0; s<3; s++){
+		 for(int s=0; s<1000; s++){
 		   for(int i=0; i<3; i++){
 		        daarige();
 		        tanmoxveva();
@@ -28,6 +28,7 @@ public class Problem7 extends SuperKarel{
 		}
 	}
 	private void tanmoxveva(){
+		
 	        if(rightIsBlocked()){
 		       turnLeft();
 		       daarige();
