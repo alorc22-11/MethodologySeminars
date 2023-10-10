ec9e1300c21b97a7af2ed8658f66871e40ddc68e
S <s>
2023-10-10 18:54:45 +0400

    {"types":"AR","runs":5}

---
+++ Problem12.java
@@ -0,0 +1,14 @@
+import java.awt.color.*;
+
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+public class Problem12 extends GraphicsProgram{
+	public void run(){
+		
+		
+		
+	}
+	
+
+}
