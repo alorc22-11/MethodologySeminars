ca23ea784549402935cb8ead8947a34c82581e6d
S <s>
2023-10-10 19:10:26 +0400

    {"types":"C","runs":25}

--- Problem12.java
+++ Problem12.java
@@ -9,11 +9,17 @@ public class Problem12 extends GraphicsProgram{
 	GRect myRect = new GRect(500,600);	
 	add(myRect, 700, 350);
 		
-	GOval myOval = new GOval(100,100);
-	add(myOval, 800, 450);
+	GOval Fanjara1 = new GOval(100,100);
+	add(Fanjara1, 800, 450);
 	
-	GOval myOVal = new GOval(100,100);
-	add(myOVal,1000, 450);
+	GOval Fanjara2 = new GOval(100,100);
+	add(Fanjara2, 1000, 450);
+	
+	GOval Fanjara3 = new GOval(100,100);
+	add(Fanjara3, 800, 600);
+	 
+	GOval Fanjara4 = new GOval(100,100);
+	add(Fanjara4, 1000, 600);
 	
 	
 	}
