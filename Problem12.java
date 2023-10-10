ff4bc782450c6c152d24cad52d4877a6240c2d55
S <s>
2023-10-10 18:41:01 +0400

    {"types":"C","runs":3}

--- Problem12.java
+++ Problem12.java
@@ -10,8 +10,8 @@ public class Problem12 extends GraphicsProgram{
 	GRect myrect = new GRect(100,200);
 	add(myrect, 100, 150);
 	
-	GRect secondRect = new GRect(100,200);
-	add(myrect, 300, 150);
+	//GRect secondRect = new GRect(100,200);
+	//add(myrect, 300, 150);
 	
 	myrect = new GRect(100,200);
 	add(myrect, 300, 150);
