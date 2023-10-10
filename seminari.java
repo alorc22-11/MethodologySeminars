b947198e8baf7b2d37420a4017324d5fe6bdc7be
S <s>
2023-10-10 18:50:03 +0400

    {"types":"AR","runs":5}

--- seminari.java
+++ seminari.java
@@ -4,7 +4,7 @@ import java.awt.color.*;
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
 	
-public class Problem12 extends GraphicsProgram{
+public class seminari extends GraphicsProgram{
     public void run(){
     	
     	
