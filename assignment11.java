c0192fc5e2df661af48ed8ce0238a8c11119a535
S <s>
2023-10-11 13:59:19 +0400

    {"types":"C","runs":0}

--- assignment11.java
+++ assignment11.java
@@ -1,4 +1,34 @@
+import stanford.karel.*;
 
-public class assignment11 {
+public class assignment11 extends SuperKarel{
+	public void run(){
+		kartanMisvla();
+		ageba();
+		saxlshiShesvla();
+		wamowola();
+	}
+
+	private void wamowola() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void saxlshiShesvla() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void ageba() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void kartanMisvla() {
+		// TODO Auto-generated method stub
+		move();
+		move();
+		mo
+	}
+	
 
 }
