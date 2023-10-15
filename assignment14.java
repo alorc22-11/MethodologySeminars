99c8fdb79ef778048cb0ca72e7d0dff372f02634
S <s>
2023-10-15 18:33:12 +0400

    {"types":"C","runs":3}

--- assignment14.java
+++ assignment14.java
@@ -2,11 +2,13 @@ import stanford.karel.SuperKarel;
 public class assignment14 extends SuperKarel {
     public void run(){
     	turnLeft();
-    	while(frontIsClear()){
+    	if(frontIsClear()){
+    		while(frontIsClear()){
     		move();
     		move();
     		turnRight();
     		move();
+    	}
     	}
     }
 }
