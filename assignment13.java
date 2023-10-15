2adf8e1dd43b59d8707128ac74f208c73f9c1ad1
S <s>
2023-10-15 13:53:17 +0400

    {"types":"C","runs":6}

--- assignment13.java
+++ assignment13.java
@@ -2,19 +2,31 @@ import stanford.karel.SuperKarel;
 
 public class assignment13 extends SuperKarel {
 	public void run() {
-		firstLine();
-		// getToSecondLine();
+		putBeepersInOrder();
+		getToSecondLine();
 		// check();
 		// goon();
 	}
 
-	private void firstLine() {
+	private void getToSecondLine() {
+		if(noBeepersPresent()){
+			turnLeft();
+			move();
+			turnRight();
+			turnAround();
+			move();
+			putBeepersInOrder();
+			
+		}
+		
+	}
+
+	private void putBeepersInOrder() {
 		putBeeper();
 		while (frontIsClear()) {
 			move();
 			if (frontIsClear()) {
 				move();
-
 				putBeeper();
 			}
 
