9390519188bec3048dce9adb0e7804cbf78dd9ea
S <s>
2023-10-31 15:12:41 +0400

    {"types":"C","runs":1}

--- Problem1.java
+++ Problem1.java
@@ -1,17 +1,10 @@
 import stanford.karel.*;
 public class Problem1 extends SuperKarel{
 	 public void run(){
-	sheavse();
-		 putBeeper();	
-
-	 }
-	 private void sheavse(){
-		 while(frontIsClear()){
-			 putBeeper();
-			 move();	
-			 
-		 }
-	
-	 }
+	move();
+	while(leftIsBlocked()){
+		move();
+	}
 	 
+}
 }
