56ab94221f8257630d2fd5117bd8b1aa91e5f9d6
S <s>
2023-10-31 18:37:33 +0400

    {"types":"C","runs":1}

--- semminari.java
+++ semminari.java
@@ -1,6 +1,14 @@
-import acm.program.*;
 import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
 public class semminari extends ConsoleProgram {
-
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run() {
+		//RandomGenerator rgen = new RandomGenerator();
+		for(int i =0; i < 100; i++){
+		int randomInt = rgen.nextInt(1,2);
+		print(randomInt);
+		}
+	    
+	}
 }
